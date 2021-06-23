f952e172cdf4418f0b06b04b4e1e2631ffe9dfb6
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-06-23 23:56:44 +0200

    Update idena-menu.sh

--- idena-menu.sh
+++ idena-menu.sh
@@ -57,6 +57,6 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
 		2) S=M2;SC;if [[ $cur == enter ]];then R;reset && bash /home/idena-aio/farm/farm-management.sh ;fi;;
 		3) S=M3;SC;if [[ $cur == enter ]];then R;reset && bash /home/idena-aio/extras/extras-menu.sh ;fi;;
 		4) S=M4;SC;if [[ $cur == enter ]];then R;reset&& bash /home/idena-aio/about.sh && exit 0 ;fi;;
-		5) S=M5;SC;if [[ $cur == enter ]];then R;exit 0 && reset && exit 0 ;fi;;
+		5) S=M5;SC;if [[ $cur == enter ]];then R;exit 0;fi;;
         
  esac;POS;done	
