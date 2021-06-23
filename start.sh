24d2daa6d4d1520b22e0f1e03872f1611b8759a9
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-06-23 23:55:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ wait
 echo " |_____|\__ _| \___||_| |_| \__ _|"
 wait
 echo
-sleep 3
+sleep 2
 
 
 echo "-------------------------------------"
@@ -22,7 +22,7 @@ echo "https://www.idena-ar.com"
 wait
 echo "-------------------------------------"
 wait
-sleep 3
+sleep 2
 
 echo please make sure that you have root access to use my script
 wait
@@ -30,7 +30,7 @@ sleep 3
 echo  created by : @egyptianpharoh
 echo  donate to my address if the tool is useful to you
 echo 0x936d0b38ac89ea0a06ea42ecf3356beed13640d9
-sleep 4
+sleep 3
 
 reset
 
