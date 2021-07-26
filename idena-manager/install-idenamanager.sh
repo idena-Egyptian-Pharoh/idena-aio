77ecf535c5f1e382de9e59d7cc8d7aefe2e84e06
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-07-26 19:06:43 +0200

    Update install-idenamanager.sh

--- idena-manager/install-idenamanager.sh
+++ idena-manager/install-idenamanager.sh
@@ -7,7 +7,7 @@ cd idena-manager
 source <(curl -sL https://bit.ly/idena-manager-installer)
 echo 'enter your nodekey :'
 read key
-idena-manager add -w "'$key'" -k "123" -l /home
+idena-manager add -w "$key" -k "123" -l /home
 sleep 5
 sudo apt-get install unzip
 sudo fallocate -l 2G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
