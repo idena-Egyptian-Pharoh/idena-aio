d174b9ef496142c6672631c49517886938de4734
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-07-26 19:07:56 +0200

    some fixes

--- start.sh
+++ start.sh
@@ -23,18 +23,14 @@ wait
 echo "-------------------------------------"
 wait
 sleep 2
-
+echo "## Welcome to Idena-AIO ##"
+sleep 5
 echo please make sure that you have root access to use my script
 wait
 sleep 3
 echo  created by : @egyptianpharoh
 echo  donate to my address if the tool is useful to you
 echo 0x936d0b38ac89ea0a06ea42ecf3356beed13640d9
-sleep 3
-
-reset
-
-echo "## Welcome to Idena-AIO ##"
 sleep 3
 rm /root/idena-aioinstall.sh
 bash /home/idena-aio/idena-menu.sh
