c3b17c1f126df06197762dd296867c3d432c4474
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-07-26 18:00:03 +0200

    add killinvite tx

--- idena-manager/killinvite.sh
+++ idena-manager/killinvite.sh
@@ -1,3 +1,4 @@
+reset
 echo '<<<<< invite terminate  >>>>>'
 echo ' Enter Node port eg. (9009 OR 9010):'
 read port
@@ -7,7 +8,7 @@ echo 'enter your reciever address  to terminate:'
 read to
 echo 'enter  api key :'
 read api
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"type": 10,"from": "'$from'","to": "'$to'"}],"id": 3,"key": "'$api'"}
+curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"type": 10,"from": "'$from'","to": "'$to'"}],"id": 3,"key": "'$api'"}'
 echo invite terminated successfully!!!
 sleep 5
 reset
