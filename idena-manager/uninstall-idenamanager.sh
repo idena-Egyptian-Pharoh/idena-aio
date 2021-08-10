92d3659ccd8e2f2fbaa55644c67d82ae9200b24f
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-08-10 15:27:24 +0200

    Update uninstall-idenamanager.sh

--- idena-manager/uninstall-idenamanager.sh
+++ idena-manager/uninstall-idenamanager.sh
@@ -2,9 +2,21 @@
 sudo rm /home/idena-node-linux-latest
 sudo rm /home/config-node1.json
 sudo rm /home/config-node2.json
+sudo rm /home/config-node3.json
+sudo rm /home/config-node4.json
+sudo rm /home/config-node5.json
+sudo rm /home/config-node6.json
+sudo rm /home/config-node7.json
+sudo rm /home/config-node8.json
 sudo rm /home/log*
 sudo rm -r /home/idena-manager
 sudo rm -r /home/datadir-node1
 sudo rm -r /home/datadir-node2
+sudo rm -r /home/datadir-node3
+sudo rm -r /home/datadir-node4
+sudo rm -r /home/datadir-node5
+sudo rm -r /home/datadir-node6
+sudo rm -r /home/datadir-node7
+sudo rm -r /home/datadir-node8
 sudo rm -r /root/.idena.config
 
