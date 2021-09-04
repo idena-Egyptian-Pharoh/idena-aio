b4fa40815c9ce26c1446d7ce9cfdfbe9b661ed54
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-09-04 21:08:45 +0200

    fix duplicated install idena-manager

--- idena-manager/install-idenamanager.sh
+++ idena-manager/install-idenamanager.sh
@@ -1,5 +1,9 @@
 #!  /usr/bin/bash
 reset
+if [ -d "/home/idena-manager ] ; then
+echo "idena manager already installed"
+idena-manager status
+else
 echo 'installing idena-manager ... '
 echo 'Please wait while downloading idenachain.db(1gb)'
 cd /home
@@ -14,4 +18,5 @@ sudo apt-get install unzip
 sudo fallocate -l 2G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
 idena-manager status
 sleep 5
+fi
 reboot
