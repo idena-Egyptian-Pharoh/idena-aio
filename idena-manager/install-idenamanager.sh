6f46b089daccff61a76819480e2010cdd0aefbda
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-10-18 21:35:37 +0200

    Update install-idenamanager.sh

--- idena-manager/install-idenamanager.sh
+++ idena-manager/install-idenamanager.sh
@@ -15,7 +15,7 @@ mkdir idena-manager
 cd idena-manager
 source <(curl -sL https://bit.ly/idena-manager-installer)
 echo 'enter your nodekey :'
-echo 'Please wait while downloading idenachain.db(1gb)'
+echo 'donate to my address if the tool is useful to you 0x936d0b38ac89ea0a06ea42ecf3356beed13640d9 '
 read key
 idena-manager add -w "$key" -k "123" -l /home
 sleep 5
