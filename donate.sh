799e3abc593d71623bcfada86834dda5ac3e9b8d
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-10-27 11:12:33 +0200

    add idenamenu to donate

--- donate.sh
+++ donate.sh
@@ -3,3 +3,4 @@ echo  created by : @egyptianpharoh
 echo  'donate to my address if the tool is useful to you'
 echo 0x936d0b38ac89ea0a06ea42ecf3356beed13640d9
 sleep 5
+bash /home/idena-aio/idena-menu.sh
