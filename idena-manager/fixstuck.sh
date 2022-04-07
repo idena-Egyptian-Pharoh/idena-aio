5f7f324940671310c38a2fc576ed0625aeefdd7a
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2022-04-07 02:28:49 +0200

    Fix txs stuck

--- idena-manager/fixstuck.sh
+++ idena-manager/fixstuck.sh
@@ -6,27 +6,9 @@ echo 'enter your  address  to fix stuck:'
 read from
 echo 'enter  api key :'
 read api
-
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 1}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 2}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 3}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 4}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 5}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 6}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 7}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 8}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 9}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 10}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 11}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 12}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 13}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 14}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 15}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 16}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 17}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 18}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 19}],"id": 9,"key": "'$api'"}'
-curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": 20}],"id": 9,"key": "'$api'"}'
+echo 'enter nonce:'
+read nonce
+curl "http://localhost:$port" -H 'Content-Type:application/json' --data '{"method": "dna_sendTransaction","params": [{"from": "'$from'","to": "'$from'","amount": "0.01","nonce": "'$nonce'"}],"id": 9,"key": "'$api'"}'
 
 sleep 5
 bash /home/idena-aio/idena-manager/more-options.sh
