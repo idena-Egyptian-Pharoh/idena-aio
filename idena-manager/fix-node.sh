bd6ebf6e601483e2e7e01c9e8ab47684f25e9bef
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2023-08-26 19:05:55 +0300

    Update fix-node.sh

--- idena-manager/fix-node.sh
+++ idena-manager/fix-node.sh
@@ -11,6 +11,7 @@ wget https://github.com/ltraveler/idenachain.db/archive/refs/heads/main.zip
 unzip main.zip
 mv /home/datadir-node$N/idenachain.db-main/* /home/datadir-node$N/idenachain.db/
 rm main.zip
+rm -r idenachain.db-main
 idena-manager enable $N
 idena-manager status
 sleep 5
